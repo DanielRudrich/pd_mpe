8d050760c9bf88d810518f3f646ca94a32bed7e7
Daniel Rudrich <mail@danielrudrich.de>
2019-04-07 12:45:15 +0200

    init

---
+++ Source/RawMidiToMPE.h
@@ -0,0 +1,212 @@
+/*
+ ==============================================================================
+ Copyright (c) 2019 - Daniel Rudrich
+ Released under GPLv3, see LICENSE.txt or <https://www.gnu.org/licenses/>.
+ ==============================================================================
+ */
+
+#pragma once
+#include <vector>
+
+class RawMidiToMPE
+{
+    static const int maxNumberOfPorts = 16;
+
+    static t_symbol* noteOnSym;
+    static t_symbol* noteOffSym;
+    static t_symbol* slideSym;
+    static t_symbol* glideSym;
+    static t_symbol* pressSym;
+public:
+    enum MidiEvent
+    {
+        noEvent, // shouldn't be sent out
+        noteOn,
+        noteOff,
+        press,
+        slide,
+        glide
+    };
+
+    RawMidiToMPE (t_outlet *outlet) : outletPtr (outlet), numPorts (maxNumberOfPorts), portFilter (-1) //
+    {
+        post ("Created RawMidiToMPE instance listening to all devices.");
+        init();
+    };
+
+
+    RawMidiToMPE (t_outlet *outlet, const int portIndex) : outletPtr (outlet), numPorts (1), portFilter (portIndex) //
+    {
+        post ("Created RawMidiToMPE instance listening to only one device.");
+        init();
+    };
+
+    ~RawMidiToMPE() {};
+
+    void init()
+    {
+        channel.resize (numPorts);
+        midiEvent.resize (numPorts);
+        firstByte.resize (numPorts);
+
+        for (auto &elem : channel)
+            elem = -1;
+
+        for (auto &elem : midiEvent)
+            elem = noEvent;
+
+        for (auto &elem : firstByte)
+            elem = -1;
+
+    }
+
+    void processRawMidi (const int byte, const int portNumber)
+    {
+        const int index = portFilter != -1 ? 0 : portNumber;
+
+        if (index < 0 || index > maxNumberOfPorts - 1 || (portFilter != -1 && portFilter != portNumber))
+            return;
+
+        auto& ch = channel[index];
+        auto& event = midiEvent[index];
+        auto& byteOne = firstByte[index];
+
+        if (event == noEvent) // listen to midi events
+        {
+            if (((byte >> 4) & 0x0F) == 0x09) // in range [144, 160[ -> noteOn
+            {
+                ch = (byte & 0x0F) + 1;
+                byteOne = -1; // reset
+                event = noteOn;
+            }
+            else if (((byte >> 4) & 0x0F) == 0x08)
+            {
+                ch = (byte & 0x0F) + 1;
+                byteOne = -1; // reset
+                event = noteOff;
+            }
+            else if (((byte >> 4) & 0x0F) == 0x0B) // control change
+            {
+                ch = (byte & 0x0F) + 1;
+                byteOne = -1; // reset
+                event = slide;
+            }
+            else if (((byte >> 4) & 0x0F) == 0x0D)
+            {
+                ch = (byte & 0x0F) + 1;
+                byteOne = -1; // reset
+                event = press;
+            }
+            else if (((byte >> 4) & 0x0F) == 0x0E)
+            {
+                ch = (byte & 0x0F) + 1;
+                byteOne = -1; // reset
+                event = glide;
+            }
+        }
+        else // already received midiEvent
+        {
+            if (byteOne == -1) // waiting for first byte
+            {
+                if (event == press) // only one byte needed
+                {
+                    //post ("Press! port: %d, channel: %d, value %d ", portNumber, ch, byte);
+                    t_atom list[4];
+
+                    SETFLOAT (list, portNumber + 1);
+                    SETFLOAT (list + 1, ch);
+                    SETSYMBOL (list + 2, pressSym);
+                    SETFLOAT (list + 3, byte);
+
+                    outlet_list (outletPtr, &s_list, 4, list);
+
+                    event = noEvent;
+                    return;
+                }
+                else if (event == slide && byte != 74) // this byte has to be 74 in order to be a slide
+                    return;
+
+                byteOne = byte;
+            }
+            else // already received first byte
+            {
+                t_atom list[5];
+
+                switch (event)
+                {
+                    case noteOn:
+                        //post ("NoteOnReceived! port: %d, channel: %d, pitch: %d, velocity %d ", portNumber, ch, byteOne, byte);
+
+                        SETFLOAT (list, portNumber + 1);
+                        SETFLOAT (list + 1, ch);
+                        SETSYMBOL (list + 2, noteOnSym);
+                        SETFLOAT (list + 3, byteOne);
+                        SETFLOAT (list + 4, byte);
+
+                        outlet_list (outletPtr, &s_list, 5, list);
+                        break;
+
+                    case noteOff:
+                        //post ("NoteOffReceived! port: %d, channel: %d, pitch: %d, velocity %d ", portNumber, ch, byteOne, byte);
+
+                        SETFLOAT (list, portNumber + 1);
+                        SETFLOAT (list + 1, ch);
+                        SETSYMBOL (list + 2, noteOffSym);
+                        SETFLOAT (list + 3, byteOne);
+                        SETFLOAT (list + 4, byte);
+
+                        outlet_list (outletPtr, &s_list, 5, list);
+                        break;
+
+                    case slide:
+                        //post ("Silde! port: %d, channel: %d,value %d ", portNumber, ch, byte);
+
+                        SETFLOAT (list, portNumber + 1);
+                        SETFLOAT (list + 1, ch);
+                        SETSYMBOL (list + 2, slideSym);
+                        SETFLOAT (list + 3, byte);
+
+                        outlet_list (outletPtr, &s_list, 4, list);
+                        break;
+
+                    case glide:
+                        //post ("Glide! port: %d, channel: %d,value %d ", portNumber, ch, byte * 127 + byteOne);
+
+                        SETFLOAT (list, portNumber + 1);
+                        SETFLOAT (list + 1, ch);
+                        SETSYMBOL (list + 2, glideSym);
+                        SETFLOAT (list + 3, byte * 127 + byteOne);
+
+                        outlet_list (outletPtr, &s_list, 4, list);
+                        break;
+
+                    default: ;
+                        //post ("Something went wrong!"); // shouldn't be reached!
+                }
+
+
+                // reset
+                byteOne = -1;
+                event = noEvent;
+            }
+        }
+    }
+
+
+private:
+    t_outlet *outletPtr = nullptr;
+
+    const int numPorts;
+    const int portFilter;
+
+    std::vector<int> channel;
+    std::vector<MidiEvent> midiEvent;
+    std::vector<int> firstByte;
+
+};
+
+t_symbol* RawMidiToMPE::noteOnSym = gensym ("noteon");
+t_symbol* RawMidiToMPE::noteOffSym = gensym ("noteoff");
+t_symbol* RawMidiToMPE::slideSym = gensym ("slide");
+t_symbol* RawMidiToMPE::glideSym = gensym ("glide");
+t_symbol* RawMidiToMPE::pressSym = gensym ("press");
---
+++ Source/mpe.cpp
@@ -0,0 +1,69 @@
+/*
+ ==============================================================================
+ Copyright (c) 2019 - Daniel Rudrich
+ Released under GPLv3, see LICENSE.txt or <https://www.gnu.org/licenses/>.
+ ==============================================================================
+ */
+
+
+
+#include "m_pd.h"
+#include <math.h>
+#include "RawMidiToMPE.h"
+
+static t_class *mpe_class;
+
+typedef struct _mpe
+{
+    t_object x_obj;
+    t_float channelFilter;
+    t_outlet *x_outlet;
+
+    RawMidiToMPE* rawMidiToMPE;
+} t_mpe;
+
+static void *mpe_new (t_floatarg f)
+{
+    t_mpe *x = (t_mpe *) pd_new (mpe_class);
+    x->channelFilter = f;
+    x->x_outlet = outlet_new (&x->x_obj, &s_float);
+
+    if (f == 0)
+        x->rawMidiToMPE = new RawMidiToMPE (x->x_outlet);
+    else
+        x->rawMidiToMPE = new RawMidiToMPE (x->x_outlet, f);
+
+    // start listening to system midiin messages
+    t_symbol *midiin = gensym ("#midiin");
+    pd_bind (&x->x_obj.ob_pd, midiin);
+
+    return x;
+}
+
+static void mpe_list (t_mpe *x, t_symbol *s, int ac, t_atom *av)
+{
+    const int byte = atom_getfloatarg (0, ac, av);
+    const int portNumber = atom_getfloatarg (1, ac, av);
+    x->rawMidiToMPE->processRawMidi (byte, portNumber);
+
+}
+
+static void mpe_free (t_mpe *x)
+{
+    // stop listening to system midiin messages
+    t_symbol *midiin = gensym ("#midiin");
+    pd_unbind (&x->x_obj.ob_pd, midiin);
+}
+
+
+extern "C" {
+
+    void mpe_setup (void)
+    {
+        mpe_class = class_new (gensym ("mpe"), (t_newmethod) mpe_new,
+                               (t_method) mpe_free, sizeof (t_mpe),
+                               CLASS_NOINLET, A_DEFFLOAT, 0);
+        class_sethelpsymbol(mpe_class, gensym("mpe"));
+        class_addlist (mpe_class, mpe_list);
+    }
+}
